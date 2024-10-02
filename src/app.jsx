68f4d3b1ae572e7fb60a0540423b278731114b2a
annah11 <hanamesfin67@gmail.com>
2024-10-03 00:20:37 +0300

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import cookies from 'universal-cookie';
+const apikey = 'https://chat-app-mychats.herokuapp.com/';
+const chatClient = StreamChat.getInstance('your-stream-chat-api-key');
 
 const app = () => {
   return (
-    <div>app</div>
+    <div>
+        <h1>mychats chat app</h1>
+    </div>
   )
 }
 
